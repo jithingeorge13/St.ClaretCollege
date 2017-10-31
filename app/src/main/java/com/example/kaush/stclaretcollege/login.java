62d82301446abdd4245eb55e16a1bfa078ed9422
jithingeorge13 <jithingeorge24@gmail.com>
2017-10-31 17:24:39 +0530

    Third Commit

--- app/src/main/java/com/example/kaush/stclaretcollege/login.java
+++ app/src/main/java/com/example/kaush/stclaretcollege/login.java
@@ -1,12 +1,21 @@
 package com.example.kaush.stclaretcollege;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
-
+    EditText uid;
+    EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +25,22 @@ public class login extends AppCompatActivity {
     public void butclick(View view) {
         Intent intent = new Intent(login.this,generalhome.class);
         startActivity(intent);
+    }
+
+    @TargetApi(21)
+    public void loginclick(View view){
+        uid = (EditText)findViewById(R.id.userid);
+        pass = (EditText)findViewById(R.id.password);
+
+        if (uid.getText().toString().equals("admin") && pass.getText().toString().equals("password")){
+            Intent intent = new Intent(login.this,generalhome.class);
+            startActivity(intent);
+        }
+        else{
+            uid.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.errorRed));
+            pass.setBackgroundTintList(ContextCompat.getColorStateList(getApplicationContext(), R.color.errorRed));
+            pass.setText("");
+
+        }
     }
 }
